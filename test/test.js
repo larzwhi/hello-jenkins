19c6a0bfa6586499de6f20f7e8281d0adeefb90e
larry white <larz@larzwerks.com>
2016-09-30 07:13:04 -0400

    changed test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins, are you not there?', done);
+    request(app).get('/').expect('hello jenkins, are you there?', done);
   });
 });
 
